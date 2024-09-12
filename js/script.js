6da653ce4b3d78315b465b1c78a4088dc46aa4bc
dava18gembozz <davaarielmaulana@gmail.com>
2024-09-12 08:05:52 +0700

    update 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // javascript
+
+function luasSegitiga() {
+    var panjangAlas = document.getElementById('panjang-alas').value;
+    var tinggi = document.getElementById('tinggi').value;
+    // if (panjangAlas == '' || tinggi == '' || panjangAlas.includes('e') || tinggi.includes('e')) {
+    //     alert("Form masih kosong bro")
+    // } else{
+    //     luasSegitiga = 0.5 * panjangAlas * tinggi;
+    //     document.getElementById('result').textContent = `Hasil Perhitungan: ${luasSegitiga}`;
+    // }   
+    luasSegitiga = 0.5 * panjangAlas * tinggi;
+    document.getElementById('result').textContent = `Hasil Perhitungan: ${luasSegitiga}`;
+}
+
+function selectionJajar() {
+    let segitiga = document.getElementById('jajar-genjang-btn');
+    let jajargenjang = document.getElementById('jajar-genjang-btn').style.backgroundColor = 'cadetblue';
+}
